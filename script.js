5eadff184e63e64d73c83cb8554754a550d2032b
Cristian Sanchez <98424716+nowchri@users.noreply.github.com>
2024-07-18 13:43:28 -0500

    script.js

--- script.js
+++ script.js
@@ -21,18 +21,19 @@ function efectoHabilidades(){
     var distancia_skills  = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >=300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
+        habilidades[0].classList.add("adobepremiere");
+        habilidades[1].classList.add("aftereffects");
         habilidades[2].classList.add("photoshop");
         habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("php");
-        habilidades[5].classList.add("csharp");
+        habilidades[4].classList.add("capcut");
+        habilidades[5].classList.add("canva");
 
         habilidades[6].classList.add("comunicacion");
         habilidades[7].classList.add("trabajo");
         habilidades[8].classList.add("creatividad");
         habilidades[9].classList.add("dedicacion");
         habilidades[10].classList.add("liderazgo");
+        habilidades[11].classList.add("edicion");
         
     }
 }
@@ -40,22 +41,7 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 }
-///contacto code
-/* numero1
-const $form = document.querySelector('#form');
-const $buttonMailto = document.querySelector('#sendM');
 
-$form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event)
-{
-    event.preventDefault();
-    const form = new FormData(this);
-    console.log(form.get('name'));
-    $buttonMailto.setAttribute('href',`mailto:cristiansantiago808@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`);
-    $buttonMailto.click();
-}
-*/
 ///PARTE 2 CON FORMSPREE
 
 const $form =document.querySelector('#form');
